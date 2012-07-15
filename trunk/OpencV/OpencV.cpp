42ad6e3886a3ab866e607d0f2672d88b68bd0ae6
rodrigo_4zevedo@hotmail.com <rodrigo_4zevedo@hotmail.com@d5417a63-283f-6ae7-0510-43af520ebf06>
2012-07-15 03:35:57 +0000

    Adicionado o tratamento de imagem para greyscale e o seu processamento.

--- trunk/OpencV/OpencV.cpp
+++ trunk/OpencV/OpencV.cpp
@@ -113,9 +113,47 @@ if (i<pFaceRectSeq->total)
 	//cvShowImage( "Source", inputImg );
 	//cvWaitKey(0);
 	cvSaveImage("C:\\imagemproc\\saida2.bmp",inputImg);
+	
+	//Face Recognition
+	// Either convert the image to greyscale, or use the existing greyscale image.
+	IplImage *imageGrey;
+	if (inputImg->nChannels == 3) {
+		imageGrey = cvCreateImage( cvGetSize(inputImg), IPL_DEPTH_8U, 1 );
+		// Convert from RGB (actually it is BGR) to Greyscale.
+		cvCvtColor( inputImg, imageGrey, CV_BGR2GRAY );
+	}
+	else {
+		// Just use the input image, since it is already Greyscale.
+		imageGrey = inputImg;
+	}
+
+	// Resize the image to be a consistent size, even if the aspect ratio changes.
+	IplImage *imageProcessed;
+	imageProcessed = cvCreateImage(cvSize(inputImg->width, inputImg->height), IPL_DEPTH_8U, 1);
+	// Make the image a fixed size.
+	// CV_INTER_CUBIC or CV_INTER_LINEAR is good for enlarging, and
+	// CV_INTER_AREA is good for shrinking / decimation, but bad at enlarging.
+	cvResize(imageGrey, imageProcessed, CV_INTER_LINEAR);
+
+	// Give the image a standard brightness and contrast.
+	cvEqualizeHist(imageProcessed, imageProcessed);
+
+	//.....  Use 'imageProcessed' for Face Recognition ....
+	// teste
+	//cvSaveImage("C:\\imagemproc\\saida3.bmp",imageGrey);
+	//cvSaveImage("C:\\imagemproc\\saida4.bmp",imageProcessed);
+
+	if (imageGrey)
+		cvReleaseImage(&imageGrey);
+	if (imageProcessed)
+		cvReleaseImage(&imageProcessed);
+	//---------------
+
 	cvReleaseImage(&inputImg);
 	if (pStorage) cvReleaseMemStorage(&pStorage);
 	if(faceCascade) cvReleaseHaarClassifierCascade(&faceCascade);
+
+
 
 }
 
